6ee0d7c387de4d4ba03b2366552000939a2baa77
Gabriel Gonzalez <Gabriel439@gmail.com>
2017-05-18 20:45:40 -0700

    Use `vector-builder`.  Fixes #88
    This improves the performance of the `vector` utility and allows it to become a
    pure `Fold`

--- default.nix
+++ default.nix
@@ -1,15 +1,16 @@
 { mkDerivation, base, bytestring, comonad, containers
 , contravariant, hashable, mwc-random, primitive, profunctors
 , stdenv, text, transformers, unordered-containers, vector
+, vector-builder
 }:
 mkDerivation {
   pname = "foldl";
-  version = "1.2.4";
+  version = "1.2.5";
   src = ./.;
   libraryHaskellDepends = [
     base bytestring comonad containers contravariant hashable
     mwc-random primitive profunctors text transformers
-    unordered-containers vector
+    unordered-containers vector vector-builder
   ];
   description = "Composable, streaming, and efficient left folds";
   license = stdenv.lib.licenses.bsd3;
