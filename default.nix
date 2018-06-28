070d4e2d12458f9d575eb56b8420fe6a1a88557a
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-06-27 20:58:01 -0700

    Version 1.4.0 → 1.4.1 (#109)

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@
 }:
 mkDerivation {
   pname = "foldl";
-  version = "1.4.0";
+  version = "1.4.1";
   src = ./.;
   libraryHaskellDepends = [
     base bytestring comonad containers contravariant hashable
