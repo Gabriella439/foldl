1c654f5d24808b224fbe87057ffc4d0cff247707
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-07-03 18:44:35 -0700

    Build against `profunctors-5.3` (#115)

---
+++ nix/foldl.nix
@@ -0,0 +1,18 @@
+{ mkDerivation, base, bytestring, comonad, containers
+, contravariant, criterion, hashable, mwc-random, primitive
+, profunctors, semigroupoids, semigroups, stdenv, text
+, transformers, unordered-containers, vector, vector-builder
+}:
+mkDerivation {
+  pname = "foldl";
+  version = "1.4.1";
+  src = ./..;
+  libraryHaskellDepends = [
+    base bytestring comonad containers contravariant hashable
+    mwc-random primitive profunctors semigroupoids semigroups text
+    transformers unordered-containers vector vector-builder
+  ];
+  benchmarkHaskellDepends = [ base criterion ];
+  description = "Composable, streaming, and efficient left folds";
+  license = stdenv.lib.licenses.bsd3;
+}
