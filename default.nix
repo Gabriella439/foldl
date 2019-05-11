334c5855da0fe37e08c306d452dd1b4f53007de0
Gabriel Gonzalez <Gabriel439@gmail.com>
2019-05-11 10:35:25 -0700

    Build against `semigroups-0.19`/`hashable-1.3` (#133)

--- default.nix
+++ default.nix
@@ -1,12 +1,8 @@
 let
-  fetchNixpkgs = import ./nix/fetchNixpkgs.nix;
+  nixpkgs = builtins.fetchTarball {
+    url = "https://github.com/NixOs/nixpkgs/archive/312a059bef8b29b4db4e73dc02ff441cab7bb26d.tar.gz";
 
-  nixpkgs = fetchNixpkgs {
-    rev = "804060ff9a79ceb0925fe9ef79ddbf564a225d47";
-
-    sha256 = "01pb6p07xawi60kshsxxq1bzn8a0y4s5jjqvhkwps4f5xjmmwav3";
-
-    outputSha256 = "0ga345hgw6v2kzyhvf5kw96hf60mx5pbd9c4qj5q4nan4lr7nkxn";
+    sha256 = "1j52yvkhw1inp6ilpqy81xv1bbwgwqjn0v9647whampkqgn6dxhk";
   };
 
   readDirectory = import ./nix/readDirectory.nix;
@@ -14,7 +10,14 @@ let
   config = {
     packageOverrides = pkgs: {
       haskellPackages = pkgs.haskellPackages.override {
-        overrides = readDirectory ./nix;
+        overrides =
+          let
+            manualOverrides = haskellPackagesNew: haskellPackagesOld: {
+            };
+
+          in
+            pkgs.lib.composeExtensions (readDirectory ./nix) manualOverrides;
+
       };
     };
   };
