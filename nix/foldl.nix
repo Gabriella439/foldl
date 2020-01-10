b62136f5c1d2246204c46571639ff83b8bdec9a2
Gabriel Gonzalez <Gabriel439@gmail.com>
2020-01-09 21:11:29 -0800

    Version 1.4.5 → 1.4.6 (#148)

--- nix/foldl.nix
+++ nix/foldl.nix
@@ -5,7 +5,7 @@
 }:
 mkDerivation {
   pname = "foldl";
-  version = "1.4.5";
+  version = "1.4.6";
   src = ./..;
   libraryHaskellDepends = [
     base bytestring comonad containers contravariant hashable
